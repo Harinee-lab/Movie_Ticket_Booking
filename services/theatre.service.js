5801d7011b8b06056ab5a5f6945708125f505032
Harinee-lab <harinisrinivasan.k@gmail.com>
2025-12-27 18:35:33 +0530

    Feat: Added pagination for theatre listing

--- services/theatre.service.js
+++ services/theatre.service.js
@@ -49,6 +49,7 @@ const deleteTheatre = async (id) => {
 const getAllTheatres = async (data) => {
   try {
     let query = {};
+    let pagination = {};
     if (data && data.city) {
       query.city = data.city;
     }
@@ -58,7 +59,14 @@ const getAllTheatres = async (data) => {
     if (data && data.name) {
       query.name = data.name;
     }
-    const response = await Theatre.find(query);
+    if (data && data.limit) {
+      pagination.limit = data.limit;
+    }
+    if (data && data.skip) {
+      let perPage = data.limit ? data.limit : 3;
+      pagination.skip = data.skip * perPage;
+    }
+    const response = await Theatre.find(query, {}, pagination);
     return response;
   } catch (error) {
     console.log(error);
