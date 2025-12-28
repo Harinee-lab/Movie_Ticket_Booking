22b29d2c2ec9a64ba4dfa3f427bed1ec8f730254
Harinee-lab <harinisrinivasan.k@gmail.com>
2025-12-28 20:53:58 +0530

    Feat:Encrypted plain password durinng register

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -35,5 +36,10 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
 const User = mongoose.model("User", userSchema);
 module.exports = User;
