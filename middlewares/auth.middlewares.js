6ffa823e0cae5134c4a1ccb8c9a8e4e77ad9eaf2
Harinee-lab <harinisrinivasan.k@gmail.com>
2025-12-31 22:21:48 +0530

    Feat:Added isAdmin authorization check

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const { errorResponseBody } = require("../utils/responsebody");
 const userService = require("../services/user.service");
+const { USER_ROLE } = require("../utils/constants");
 const validateSignupRequest = async (req, res, next) => {
   if (!req.body.name) {
     errorResponseBody.err = "Name of the user not present in the request";
@@ -64,9 +65,50 @@ const validateResetPasswordRequest = async (req, res, next) => {
   }
   next();
 };
+const isAdmin = async (req, res, next) => {
+  const user = await userService.getUserById(req.user);
+  if (user.userRole != USER_ROLE.ADMIN) {
+    errorResponseBody.err =
+      "User is not an admin, cannot proceed with the request";
+    return res.status(401).json(errorResponseBody);
+  }
+  next();
+};
+const isClient = async (req, res, next) => {
+  const user = await userService.getUserById(req.user);
+  if (user.userRole != USER_ROLE.CLIENT) {
+    errorResponseBody.err =
+      "User is not a client, cannot proceed with the request";
+    return res.status(401).json(errorResponseBody);
+  }
+  next();
+};
+const isAdminOrClient = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+      errorResponseBody.err = "No token provided";
+      return res.status(403).json(errorResponseBody);
+    }
+    const decoded = jwt.verify(token, process.env.AUTH_KEY);
+    const user = await userService.getUserById(decoded.id);
+    if (user.userRole !== USER_ROLE.admin && user.userRole !== USER_ROLE.client) {
+      errorResponseBody.err = "User is neither an admin nor a client";
+      return res.status(403).json(errorResponseBody);
+    }
+    req.userId = decoded.id; 
+    next();
+  } catch (error) {
+    errorResponseBody.err = error.message;
+    return res.status(500).json(errorResponseBody);
+  }
+};
 module.exports = {
   validateSignupRequest,
   validateSignInRequest,
   isAuthenticated,
   validateResetPasswordRequest,
+  isAdmin,
+  isClient,
+  isAdminOrClient,
 };
