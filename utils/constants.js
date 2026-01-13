4636c12b619adb4eca9f51478a1a86b0523ccdf9
Harinee-lab <harinisrinivasan.k@gmail.com>
2026-01-13 10:53:13 +0530

    Feat:Added booking schema with mongoose

--- utils/constants.js
+++ utils/constants.js
@@ -11,15 +11,20 @@ const USER_ROLE = {
 const STATUS_CODES = {
   OK: 200,
   INTERNAL_SERVER_ERROR: 500,
-    CREATED: 201,
-    UNAUTHORIZED: 401,
-    NOT_FOUND: 404,
-    FORBIDDEN: 403,
-    BAD_REQUEST: 400,
-    
+  CREATED: 201,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  FORBIDDEN: 403,
+  BAD_REQUEST: 400,
+};
+const BOOKING_STATUS = {
+  processing: "IN_PROCESS",
+  cancelled: "CANCELLED",
+  successful: "SUCCESSFUL",
 };
 module.exports = {
   USER_ROLE,
   USER_STATUS,
   STATUS_CODES,
+  BOOKING_STATUS,
 };
